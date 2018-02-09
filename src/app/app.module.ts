b7f5d25d75efeab6ca7d675d1c01f53a6748cc6d
nhatmai-dev <nhatmai.dev@gmail.com>
2018-02-09 17:59:19 +0700

    Implement first GUI

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,10 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { MatButtonModule, MatInputModule } from '@angular/material';
+
+import { FormsModule } from '@angular/forms'
+import { RoundProgressModule } from 'angular-svg-round-progressbar';
 
 
 @NgModule({
@@ -10,7 +14,11 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    RoundProgressModule,
+    MatButtonModule,
+    MatInputModule
   ],
   providers: [],
   bootstrap: [AppComponent]
